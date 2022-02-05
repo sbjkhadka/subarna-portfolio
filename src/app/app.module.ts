daf40d4e43a0c20ed642d2a7659c9f350a7fa9f4
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-02-05 02:12:28 -0500

    Generated empty components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [					
+    AppComponent,
+      HeaderComponent,
+      ResumeComponent,
+      PortfolioComponent,
+      FooterComponent,
+      NavbarComponent,
+      AboutComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
