83befbe03d0ae8d549854147fb940052a0c0527f
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-02-06 02:08:19 -0500

    Fragment routing issues resolved

--- src/app/header/navbar/navbar.component.ts
+++ src/app/header/navbar/navbar.component.ts
@@ -8,7 +8,6 @@ import { Component, HostListener, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   scrolling = false;
-  
   constructor() { }
 
   ngOnInit() {
@@ -18,8 +17,16 @@ export class NavbarComponent implements OnInit {
   scrollFunction() {
     if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
       this.scrolling = true;
+      
     } else {
       this.scrolling = false;
+    }
+    // Fix for about not working after returning back after scroll to about
+    // 1. Click down arrow(to navigate to about)
+    // 2. Scroll to top of the page
+    // 3. Click on about it won't work, but it will work after introducing this condition
+    if (window.pageYOffset === 0) {
+      window.location.hash = '';
     }
   }
 
