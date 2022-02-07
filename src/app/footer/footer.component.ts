33020b9b3df63f14719df5be79383e941679db26
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-02-07 17:03:52 -0500

    half footer done

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Social } from '../Models/social.interface';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  @Input() social: Social = {};
+  year = new Date().getFullYear();
+  name = 'Subarna';
   constructor() { }
 
   ngOnInit() {
