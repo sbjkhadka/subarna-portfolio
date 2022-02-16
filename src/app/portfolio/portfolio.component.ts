0da26ca7eb5b4d345351b4f417c2977ed2132c75
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-02-16 04:12:51 -0500

    Added common project

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -13,6 +13,7 @@ export class PortfolioComponent implements OnInit {
     { link: 'https://github.com/sbjkhadka/subarna-portfolio', language: 'Angular', description: 'Portfolio', src: '../../assets/images/projects/portfolio.png' },
     { link: 'https://github.com/sbjkhadka/Group1COMP308Project', language: 'React', description: 'Nurse', src: '../../assets/images/projects/nurses.jpeg' },
     { link: 'https://sbjkhadka.github.io/cencol_css_first/', language: 'HTML/CSS', description: 'First Semester Project', src: '../../assets/images/projects/realtor.png' },
+    { link: 'https://angular-common-patterns.herokuapp.com/', language: 'Angular', description: 'Angular Common Patterns', src: '../../assets/images/projects/common.png' },
     ];
   constructor() { }
 
