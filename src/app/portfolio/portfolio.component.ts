4a8f813fbcdb7c1e38cd860ee6bc8d4930808c45
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-02-21 14:39:38 -0500

    Added projects

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -5,19 +5,58 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent {
 
   projects = [
-    { link: 'https://github.com/sbjkhadka/COMP-Two-Three-One-Frontend', language: 'Angular', description: 'Grocery List Generator', src: '../../assets/images/projects/grocery.png' },
-    { link: 'https://github.com/sbjkhadka/JQuery-Live-Search', language: 'JQuery', description: 'Search within text', src: '../../assets/images/projects/live_search.png' },
-    { link: 'https://github.com/sbjkhadka/subarna-portfolio', language: 'Angular', description: 'Portfolio', src: '../../assets/images/projects/portfolio.png' },
-    { link: 'https://github.com/sbjkhadka/Group1COMP308Project', language: 'React', description: 'Nurse', src: '../../assets/images/projects/nurses.jpeg' },
-    { link: 'https://sbjkhadka.github.io/cencol_css_first/', language: 'HTML/CSS', description: 'First Semester Project', src: '../../assets/images/projects/realtor.png' },
-    { link: 'https://angular-common-patterns.herokuapp.com/', language: 'Angular', description: 'Angular Common Patterns', src: '../../assets/images/projects/common.png' },
+    {
+      link: 'https://angular-common-patterns.herokuapp.com/booking',
+      language: 'Angular',
+      description: 'Ticket Selector',
+      src: '../../assets/images/projects/ticket_selector.png'
+    },
+    {
+      link: 'https://github.com/sbjkhadka/subarna-portfolio',
+      language: 'Angular',
+      description: 'Portfolio',
+      src: '../../assets/images/projects/portfolio.png'
+    },
+    {
+      link: 'https://github.com/sbjkhadka/COMP-Two-Three-One-Frontend',
+      language: 'Angular',
+      description: 'Grocery List Generator',
+      src: '../../assets/images/projects/grocery.png'
+    },
+    {
+      link: 'https://angular-common-patterns.herokuapp.com/combine-latest',
+      language: 'Angular',
+      description: 'RxJS E-Commerce Filter',
+      src: '../../assets/images/projects/ecommerce.png'
+    },
+    {
+      link: 'https://angular-common-patterns.herokuapp.com/inline-editing',
+      language: 'Angular',
+      description: 'Table Inline Editing',
+      src: '../../assets/images/projects/common.png'
+    },
+    {
+      link: 'https://github.com/sbjkhadka/Group1COMP308Project',
+      language: 'React',
+      description: 'Nurse',
+      src: '../../assets/images/projects/nurses.jpeg'
+    },
+    {
+      link: 'https://github.com/sbjkhadka/JQuery-Live-Search',
+      language: 'JQuery',
+      description: 'Search within text',
+      src: '../../assets/images/projects/live_search.png'
+    },
+    {
+      link: 'https://sbjkhadka.github.io/cencol_css_first/',
+      language: 'HTML/CSS',
+      description: 'First Semester Project',
+      src: '../../assets/images/projects/realtor.png'
+    },
     ];
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
