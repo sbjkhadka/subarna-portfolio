65eb0f34147539109d0bfb50d54b7a27edfcf1f3
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-04-09 19:41:28 -0400

    Resume updated

--- src/app/services/download.service.ts
+++ src/app/services/download.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -7,13 +7,13 @@ import { Observable } from 'rxjs';
 })
 export class DownloadService {
 
-resumeURL = 'https://github.com/sbjkhadka/COMP397-W2021-Lesson8a/blob/main/Resume_Subarna.docx?raw=true';
+  resumeURL = 'https://drive.google.com/file/d/1X-ZKqDp_oDuLwwLKH7PDt0qJNnyknaNV/view?usp=sharing';
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-downloadResume(): Observable<Blob> {
-  // return this.http.get(this.resumeURL, {responseType: 'blob'})
-  return this.http.get('../../assets/temp/Resume_Subarna.pdf', { responseType: 'blob' })
-}
+  downloadResume(): Observable<Blob> {
+    // return this.http.get(this.resumeURL, { responseType: 'blob' })
+    return this.http.get('../../assets/temp/Resume_Subarna.pdf', { responseType: 'blob' })
+  }
 
 }
